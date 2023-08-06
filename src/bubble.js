6985cc77da2381cb94d0523ee214feea11ac25e2
Maijs Garais <mai-soup@mailfence.com>
2023-08-06 19:37:42 +0100

    add borders, add tests for border style for single-line messages

--- src/bubble.js
+++ src/bubble.js
@@ -7,6 +7,49 @@ const speechDelimiters = {
   oneLine: ['<', '>'],
 };
 
+function createBubble(message, delimiters) {
+  const result = [];
+  const lines = message.split('\n');
+
+  const longestLine = Math.max(...lines.map(line => line.length));
+
+  // top border
+  result.push(` ${'-'.repeat(longestLine + 2)}`);
+
+  // middle
+  if (lines.length === 1) {
+    result.push(
+      `${delimiters.oneLine[0]} ${lines[0]} ${delimiters.oneLine[1]}`
+    );
+  } else {
+    lines.forEach((line, index) => {
+      if (index === 0) {
+        // first line of text, use beginning delimiters
+        result.push(
+          `${delimiters.beginning[0]} ${line} ${delimiters.beginning[1]}`
+        );
+      } else if (index === lines.length - 1) {
+        // last line of text, pad and use end delimiters
+        result.push(
+          `${delimiters.end[0]} ${line.padEnd(longestLine, ' ')} ${
+            delimiters.end[1]
+          }`
+        );
+      } else {
+        // all other lines, use middle delimiters
+        result.push(`${delimiters.middle[0]} ${line} ${delimiters.middle[1]}`);
+      }
+    });
+  }
+
+  // bottom border
+  result.push(` ${'-'.repeat(longestLine + 2)}`);
+
+  return result.join('\n');
+}
+
 module.exports.say = (message, lineWidth = 80) => {
-  return wordWrap(message, { width: lineWidth });
+  // subtract 4 to account for border
+  const wrapped = wordWrap(message, { width: lineWidth - 4 });
+  return createBubble(wrapped, speechDelimiters);
 };
--- tests/bubble.test.js
+++ tests/bubble.test.js
@@ -10,13 +10,25 @@ it('splits long message into multiple lines by word with default lineWidth', () 
   const result = bubble.say('woof '.repeat(40)).split('\n').length;
   // 5 chars * 40 = 200 chars, should be split into 3 lines
   // as default splits at 80 chars
-  const expected = 3;
+  const expected = 3 + 2; // add 2 to account for border
   expect(result).toBe(expected);
 });
 
 it('splits long message into multiple lines by word with custom lineWidth', () => {
   const result = bubble.say('woof '.repeat(40), 60).split('\n').length;
   // 5 chars * 40 = 200 chars, should be split into 4 lines
-  const expected = 4;
+  const expected = 4 + 2; // add 2 to account for border
   expect(result).toBe(expected);
+});
+
+it('uses correct border style for single-line messages', () => {
+  const result = bubble.say('woof').split('\n');
+
+  // should be of length 3
+  expect(result.length).toBe(3);
+
+  // check all lines match expected style
+  expect(result[0]).toMatch(/^ -+$/);
+  expect(result[1]).toMatch(/^< .+ >$/);
+  expect(result[2]).toMatch(/^ -+$/);
 });
