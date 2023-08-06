cd24586509ecb33083682108f6b5d4788e9d4e32
Maijs Garais <mai-soup@mailfence.com>
2023-08-06 21:30:02 +0100

    fix lines not being padded properly, add demo in index.js

--- src/bubble.js
+++ src/bubble.js
@@ -23,21 +23,20 @@ function createBubble(message, delimiters) {
     );
   } else {
     lines.forEach((line, index) => {
+      const paddedLine = line.padEnd(longestLine, ' ');
       if (index === 0) {
         // first line of text, use beginning delimiters
         result.push(
-          `${delimiters.beginning[0]} ${line} ${delimiters.beginning[1]}`
+          `${delimiters.beginning[0]} ${paddedLine} ${delimiters.beginning[1]}`
         );
       } else if (index === lines.length - 1) {
         // last line of text, pad and use end delimiters
-        result.push(
-          `${delimiters.end[0]} ${line.padEnd(longestLine, ' ')} ${
-            delimiters.end[1]
-          }`
-        );
+        result.push(`${delimiters.end[0]} ${paddedLine} ${delimiters.end[1]}`);
       } else {
         // all other lines, use middle delimiters
-        result.push(`${delimiters.middle[0]} ${line} ${delimiters.middle[1]}`);
+        result.push(
+          `${delimiters.middle[0]} ${paddedLine} ${delimiters.middle[1]}`
+        );
       }
     });
   }
@@ -50,6 +49,6 @@ function createBubble(message, delimiters) {
 
 module.exports.say = (message, lineWidth = 80) => {
   // subtract 4 to account for border
-  const wrapped = wordWrap(message, { width: lineWidth - 4 });
+  const wrapped = wordWrap(message, { width: lineWidth - 4, indent: '' });
   return createBubble(wrapped, speechDelimiters);
 };
